4016d3a9f3fa54543a1a4920629c315b0f29449c
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-11-05 09:03:23 -0500

    Added routes to the app

---
+++ src/components/Layouts/LayoutMain.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+function LayoutMain() {
+  return (
+    <div>
+      LayoutMain
+      <Outlet />
+    </div>
+  );
+}
+
+export default LayoutMain;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LayoutMain from './components/Layouts/LayoutMain.tsx';
+import Home from './pages/Home/Home.tsx';
+import Cart from './pages/Cart/Cart.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LayoutMain />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+    ],
+  },
+  {},
+  {},
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>
 );
