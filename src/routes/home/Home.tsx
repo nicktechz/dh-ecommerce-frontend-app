dbb5c5e2ffe1bde6f05b1ff5d2f1503e4bd89663
Nicolas Rueda <164514047+nicktechz@users.noreply.github.com>
2024-11-06 17:58:10 -0500

    Added latest section of the home page

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,4 +1,4 @@
-import { Candy, Heart } from 'lucide-react';
+import { Candy, Crown, Heart } from 'lucide-react';
 import { IProducts } from '../../config/types/types';
 import Hero from './components/Hero/Hero';
 import ProductsShowcase from './components/ProductsShowcase/ProductsShowcase';
@@ -73,19 +73,92 @@ function Home() {
         'Pan dulce con aroma y sabor a canela, suave y esponjoso, perfecto para disfrutar en cualquier momento.',
     },
   ];
+  const traditionalProducts: IProducts[] = [
+    {
+      key: 1,
+      name: 'Pan de leche',
+      price: '20.500',
+      imgUrl:
+        'https://res.cloudinary.com/dyjzwx82w/image/upload/v1730932862/cdhf69xj6dplevpqjs9a.jpg',
+      shortDescription:
+        'Pan suave y esponjoso con un toque de leche, ideal para acompañar desayunos y meriendas.',
+    },
+    {
+      key: 2,
+      name: 'Pan integral',
+      price: '19.000',
+      imgUrl:
+        'https://res.cloudinary.com/dyjzwx82w/image/upload/v1730932962/j7e6ritazxqmjtu9m8ci.jpg',
+      shortDescription:
+        'Pan de sabor suave y textura densa, elaborado con harina integral para una opción más saludable y deliciosa.',
+    },
+    {
+      key: 3,
+      name: 'Pan integral de chocolate',
+      price: '22.000',
+      imgUrl:
+        'https://res.cloudinary.com/dyjzwx82w/image/upload/v1730933110/s9fhov7tw3klqcjdbtlv.jpg',
+      shortDescription:
+        'Pan integral con un toque de chocolate, combinando lo saludable con un sabor dulce y delicioso.',
+    },
+    {
+      key: 4,
+      name: 'Pan blanco de pesto',
+      price: '24.500',
+      imgUrl:
+        'https://res.cloudinary.com/dyjzwx82w/image/upload/v1730933181/pv7izbss6ekosgvguan9.jpg',
+      shortDescription:
+        'Pan suave con un delicioso toque de pesto, perfecto para acompañar pastas o disfrutar solo',
+    },
+    {
+      key: 5,
+      name: 'Pan de arequipe',
+      price: '22.000',
+      imgUrl:
+        'https://res.cloudinary.com/dyjzwx82w/image/upload/v1730933319/a84tucegnwnlo1fkbyjv.jpg',
+      shortDescription:
+        'Esponjoso pan relleno de arequipe cremoso, una deliciosa combinación de suavidad y dulzura.',
+    },
+    {
+      key: 6,
+      name: 'Pan de frutas cristalizadas',
+      price: '22.000',
+      imgUrl:
+        'https://res.cloudinary.com/dyjzwx82w/image/upload/v1730933385/c6gq4qg9unn3wz0nztlh.jpg',
+      shortDescription:
+        'Pan dulce y esponjoso con trozos de frutas cristalizadas, ofreciendo un toque frutal y delicioso en cada bocado.',
+    },
+  ];
   return (
     <>
       <Hero />
-      <section className={style.products_month_box}>
+      <section
+        className={`${style.products_container} ${style.products_dark_bg}`}
+        style={{ padding: 0 }}
+      >
         <TickerText color="Light" text="FAVORITOS DEL MES" icon={<Heart />} />
         <div className={style.products_inner_container}>
           <ProductsShowcase products={productsFavourites} color="Light" />
         </div>
       </section>
-      <section className={style.products_pankas}>
+      <section
+        className={`${style.products_container} ${style.products_light_bg}`}
+      >
         <TickerText color="Dark" text="ANTOJO DULCE" icon={<Candy />} />
         <div className={style.products_inner_container}>
           <ProductsShowcase products={sweetProducts} color="Dark" />
+        </div>
+      </section>
+      <section
+        className={`${style.products_container} ${style.products_dark_bg}`}
+      >
+        <TickerText
+          color="Light"
+          text="CLÁSICOS PERO PODEROSOS"
+          icon={<Crown />}
+        />
+        <div className={style.products_inner_container}>
+          <ProductsShowcase products={traditionalProducts} color="Light" />
         </div>
       </section>
     </>
